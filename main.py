713b4a9a5118641b23686af322683e3b53ca1ebc
pimap <rr28_yosizumi@hotmail.com>
2021-11-25 23:00:29 +0900

    loggingテスト

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from .src import locator
-from .src.outputter import csv_outputter
+from src import locator
+from src.outputter import csv_outputter
 
+MAXCOUNT = 100
 def main():
     loc = locator.Locator()
-    loc.setup()
     out = csv_outputter.CsvOutputter('./test.csv')
+    count = 0
     for data in loc.logging():
         out.put_data(data)
+        count += 1
+        if count > MAXCOUNT:
+            break
     out.finish()
 
 if __name__ == '__main__':
